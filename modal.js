8cbec45f689b6184acd298ba319e48442297fb11
zakariaeelhassad <zakariaelhassad031@gmail.com>
2024-11-02 19:41:58 +0100

    Modify HTML structure and update JavaScript file for improved functionality

--- modal.js
+++ modal.js
@@ -1,191 +1,107 @@
 // ajout et l'affichage de task
 const modal = document.getElementById("addModal");
+const modal1 = document.getElementById("addModal1");
 const roturn = document.getElementById("rotorn");
 const addForm = document.getElementById("task-modal");
+const moltiplrModal = document.getElementById("moltiplrModal");
+const titre = document.getElementById("titre");
+const date = document.getElementById("date");
+const statut = document.getElementById("statut");
+const priorite = document.getElementById("priorite");
+const description = document.getElementById("description");
+// m
+const titre1 = document.getElementById("titre1");
+const date1 = document.getElementById("date1");
+const statut1 = document.getElementById("statut1");
+const priorite1 = document.getElementById("priorite1");
+const description1 = document.getElementById("description1");
+// 
 const todo = document.getElementById("todo");
 const doing = document.getElementById("doing");
 const done = document.getElementById("done");
 
+//conteurs 
+let conteur_todo = document.getElementById("conteur-todo");
+let C_todo = 0;
+let conteur_doing = document.getElementById("conteur-doing");
+let C_doing = 0;
+let conteur_done = document.getElementById("conteur-done");
+let C_done = 0;
 
 
 function toggleModal() {
-        modal.classList.remove("hidden");
-        setTimeout(() => {
-            modal.classList.add("show");
-        }, 10); 
-    
+    modal.classList.remove("hidden");
+    setTimeout(() => {
+        modal.classList.add("show");
+    }, 10);
+
 }
 
 function rutornpage() {
-    modal.classList.add("hidden");    
+    modal.classList.add("hidden");
 }
 
-// function addTask(){
-//     if(document.querySelector("#titre").value ==''){
-//         alert("enter titre");
-//     }
-//     else if (document.querySelector(document.querySelector()).value == ''){
-//         alert("enter date");
-//     }
-//     else if (document.querySelector("#description").value=''){
-//         alert("enter description");
-//     }
-//     else if (document.querySelector("#priorite").value=''){
-//         alert("enter priorite");
-//     }
-//      else if (document.querySelector("#statut").value=''){
-//             alert("enter statut");
-//     }
-// }
+function toggleModal1() {
+    modal1.classList.remove("hidden");
+    setTimeout(() => {
+        modal1.classList.add("show");
+    }, 10);
 
-// add task
-// const titre = document.getElementById("titre");
-// const date = document.getElementById("date");
-// const statut=document.getElementById("statut");
-// const description = document.getElementById("description");
-// const todo = document.getElementById("todo");
-// const doing = document.getElementById("doing");
-// const done = document.getElementById("done");
+}
 
+function rutornpage1() {
+    modal1.classList.add("hidden");
+}
 
-// document.getElementById("add-task-btn").addEventListener("click",(e) => {
-//     e.preventDefault();
-    
-//     const titre = document.getElementById("titre");
-//     const date = document.getElementById("date");
-//     const statut=document.getElementById("statut");
-//     const description = document.getElementById("description");
-
-//     if(titre.value === ""  || date.value === "" || statut.value === "" || description.value === ""){
-//         alert("entre les anformation");
-//     }
-//     else{
-//         const row = document.createElement("div");
-//             row.innerHTML = `
-//             <div id="task" class="flex flex-wrap justify-center gap-5 border border-l-8 border-red-700  p-2 rounded-3xl w-[100%] my-1 ">
-//                 <div onclick="showFullText()" class="col-span-1 border-4 border-black hover:border-blue-300 px-8 py-2 rounded-3xl my-2">
-//                     <p>
-//                         <span>Titre :</span>
-//                         <span id="myspan">${titre.value}</span>
-//                     </p>
-//                     <p>
-//                         <span>Date :</span>
-//                         <span>${date.value}</span>
-//                     </p>
-//                 </div>
-//                 <div class="grid grid-cols-1">
-//                     <button type="button" class="text-white bg-red-700 hover:bg-red-400 rounded-3xl border border-x-4 border-red-700 hover:border-red-400 mb-2"><img src="../img/poubelle.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
-//                     <button type="button" class="text-white bg-green-700 hover:bg-green-400 rounded-3xl border border-x-4 border-green-700 hover:border-green-400 "><img src="../img/editer.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
-//                 </div>
-//             </div>
-//             `
-//     }
-//     if (statut.value === "todo") {
-//         todo.appendChild(row);
-//     } else if (statut.value === "doing") {
-//         doing.appendChild(row);
-//     } else if (statut.value === "done") {
-//         done.appendChild(row);
-//     }
-//     addmodel.reset();
-//     addmodel.classList.add("hidden"); 
-//     modal.classList.add("hidden"); 
-// });
-
-
-// const span = document.getElementById("myspan");
-// const fullText = span.textContent;
-// const shortText = fullText.slice(0, 5) + "...";
-// span.textContent = shortText;
-// function showFullText() {
-//     span.textContent = fullText;
-// }
-
-// let toutModel = [];
-
-
-// function ajout(toutModel){
-//     todo.innerHTML="";
-//     doing.innerHTML="";
-//     dome.innerHTML="";
-//     toutModel.forEach(enAjout => {
-//         if(enAjout.statut =='todo'){
-//              todo.innerHTML = `
-//              <div id="task" class="flex flex-wrap justify-center gap-5 border border-l-8 border-red-700  p-2 rounded-3xl w-[100%] my-1 ">
-//                  <div onclick="showFullText()" class="col-span-1 border-4 border-black hover:border-blue-300 px-8 py-2 rounded-3xl my-2">
-//                      <p>
-//                          <span>Titre :</span>
-//                          <span id="myspan">${titre}</span>
-//                      </p>
-//                      <p>
-//                          <span>Date :</span>
-//                          <span>${date}</span>
-//                      </p>
-//                  </div>
-//                  <div class="grid grid-cols-1">
-//                      <button type="button" class="text-white bg-red-700 hover:bg-red-400 rounded-3xl border border-x-4 border-red-700 hover:border-red-400 mb-2"><img src="../img/poubelle.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
-//                      <button type="button" class="text-white bg-green-700 hover:bg-green-400 rounded-3xl border border-x-4 border-green-700 hover:border-green-400 "><img src="../img/editer.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
-//                  </div>
-//                 <div>
-//                     <button onclick="showFullText()" type="button" class="text-white bg-red-700 hover:bg-red-400 rounded-3xl border border-x-4 border-red-700 hover:border-red-400 mb-2"><img src="../img/poubelle.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
-//                 </div>
-//             </div>
-//             `
-//         }
-//     })
-        
-//     }
-
-// ajout(toutModel);
-// document.getElementById('addModel').addEventListener('submit',function(event){
-//     this.hidden.classList.add('hidden');
-//     event.preventDefault();
-//     const titre = document.getElementById('titre').value;
-//     const date = document.getElementById('date').value;
-//     const statut=document.getElementById('statut').value;
-//     const description = document.getElementById('description').value;
-//     const enAjout = {
-//         titre: titre,
-//         date: date,
-//         description: description,
-//         statut: statut,
-//     };
-//     toutModel.push(enAjout);
-//     ajout(toutModel);
-// });
-
-
+let datatask = [];
 
 
 addForm.addEventListener("submit", (event) => {
     event.preventDefault();
-const titre = document.getElementById("titre").value;
-const date = document.getElementById("date").value;
-const statut=document.getElementById("statut").value;
-const priorite=document.getElementById("priorite").value;
-const description = document.getElementById("description").value;
+    let newtask = {
+        titre: titre.value,
+        date: date.value,
+        description: description.value,
+        statut: statut.value,
+        priorite: priorite.value,
+        
+    }
+    // localStorage.setItem(newtask.titre, JSON.stringify(newtask));
+    // let num = datatask.length;
 
-const taskElement = document.createElement("div");
+    // console.log(localStorage.getItem());
+    if (newtask.titre === "") {
+        alert("entre le titre");
+    } else if (newtask.date === "") {
+        alert("entre la date");
+    } else if (newtask.description === "") {
+        alert("entre la description");
+    }else{
+    cleartask();
 
-    taskElement.classList.add("flex","flex-wrap", "justify-center", "gap-5","p-4", "border","border-l-8","p-2", "rounded-3xl" ,"w-[100%]" ,"my-1");
 
-    if (priorite.includes("p1")) {
+    const taskElement = document.createElement("div");
+
+    taskElement.classList.add("flex", "flex-wrap", "justify-center", "gap-5", "p-4", "border", "border-l-8", "p-2", "rounded-3xl", "w-[100%]", "my-1");
+
+    if (newtask.priorite === "p1") {
         taskElement.classList.add("border-red-700");
-    } else if (priorite.includes("p2")) {
+    } else if (newtask.priorite === "p2") {
         taskElement.classList.add("border-orange-400");
-    } else if (priorite.includes("p3")) {
+    } else if (newtask.priorite === "p2") {
         taskElement.classList.add("border-green-700");
     }
+    
 
-taskElement.innerHTML = `
-                <div onclick="showFullText()" class="col-span-1 border-4 border-black hover:border-blue-300 px-8 py-2 rounded-3xl my-2">
+    taskElement.innerHTML = `
+                <div class="col-span-1 border-4 border-black hover:border-blue-300 px-8 py-2 rounded-3xl my-2">
                     <p>
                         <span>Titre :</span>
-                        <span id="myspan">${titre}</span>
+                        <span class="spanTitre">${newtask.titre}</span>
                     </p>
                     <p>
                         <span>Date :</span>
-                        <span>${date}</span>
+                        <span  class="spanDate">${newtask.date}</span>
                     </p>
                 </div>
                 <div class="grid grid-cols-1">
@@ -194,21 +110,163 @@ taskElement.innerHTML = `
                 </div>
                 `
 
-                if (statut === "todo") {
-                    todo.appendChild(taskElement);
-                } else if (statut === "doing") {
-                    doing.appendChild(taskElement);
-                } else if (statut === "done") {
-                    done.appendChild(taskElement);
-                }
-                addForm.reset();
-                addForm.classList.add("hidden"); 
-                modal.classList.add("hidden"); 
+    if (newtask.statut === "todo") {
+        todo.appendChild(taskElement);
+        C_todo++;
+        conteur_todo.innerHTML = C_todo;
+    } else if (newtask.statut === "doing") {
+        doing.appendChild(taskElement);
+        C_doing++;
+        conteur_doing.innerText = C_doing;
+    } else if (newtask.statut === "done") {
+        done.appendChild(taskElement);
+        C_done++;
+        conteur_done.innerText = C_done;
+    }
+
+    
+    modal.classList.add("hidden"); 
+}
+
 });
+
+
+// add moltiple model
+
+
+moltiplrModal.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newtask = {
+        titre1: titre1.value,
+        date1: date1.value,
+        description1: description1.value,
+        statut1: statut1.value,
+        priorite1: priorite1.value,
+        
+    }
+    // localStorage.setItem(newtask.titre, JSON.stringify(newtask));
+    // let num = datatask.length;
+
+    // console.log(localStorage.getItem());
+    if (newtask.titre1 === "") {
+        alert("entre le titre");
+    } else if (newtask.date1 === "") {
+        alert("entre la date");
+    } else if (newtask.description1 === "") {
+        alert("entre la description");
+    }else{
+    cleartask1();
+
+
+    const taskElement1 = document.createElement("div");
+
+    taskElement1.classList.add("flex", "flex-wrap", "justify-center", "gap-5", "p-4", "border", "border-l-8", "p-2", "rounded-3xl", "w-[100%]", "my-1");
+
+    if (newtask.priorite1 === "p1") {
+        taskElement1.classList.add("border-red-700");
+    } else if (newtask.priorite1 === "p2") {
+        taskElement1.classList.add("border-orange-400");
+    } else if (newtask.priorite1 === "p2") {
+        taskElement1.classList.add("border-green-700");
+    }
+
+    taskElement1.innerHTML = `
+                <div class="col-span-1 border-4 border-black hover:border-blue-300 px-8 py-2 rounded-3xl my-2 ">
+                    <p>
+                        <span>Titre :</span>
+                        <span class="spanTitre">${newtask.titre1}</span>
+                    </p>
+                    <p>
+                        <span>Date :</span>
+                        <span  class="spanDate">${newtask.date1}</span>
+                    </p>
+                </div>
+                <div class="grid grid-cols-1">
+                    <button onClick="deletePost(this)" type="button" class="text-white bg-red-700 hover:bg-red-400 rounded-3xl border border-x-4 border-red-700 hover:border-red-400 mb-2"><img src="../img/poubelle.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
+                    <button onClick="editPost(this)" type="button" class="text-white bg-green-700 hover:bg-green-400 rounded-3xl border border-x-4 border-green-700 hover:border-green-400 "><img src="../img/editer.png" alt="" style="width: 15px; height: 15px; margin: 10px;"></button>
+                </div>
+                `
+    if (newtask.statut1 === "todo") {
+        todo.appendChild(taskElement1);
+        C_todo++;
+        conteur_todo.innerHTML = C_todo;
+    } else if (newtask.statut1 === "doing") {
+        doing.appendChild(taskElement1);
+        C_doing++;
+        conteur_doing.innerText = C_doing;
+    } else if (newtask.statut1 === "done") {
+        done.appendChild(taskElement1);
+        C_done++;
+        conteur_done.innerText = C_done;
+    }
+    moltiplrModal.reset();
+    moltiplrModal.classList.add("hidden"); 
+    modal1.classList.add("hidden"); 
+
+}
+
+});
+
+
+
+// clear one task
+
+function cleartask(){
+    titre.value = '' ;
+    date.value = '';
+    description.value = '';
+}
+
+// clear multiple task
+function cleartask1(){
+    titre1.value = '' ;
+    date1.value = '';
+    description1.value = '';
+}
+
+
+// delet task
 
 let deletePost = (e) => {
     e.parentElement.parentElement.remove();
-  };
+};
+
+// edit task
+let editPost = (e) => {
+    const edit = e.parentElement.parentElement;
+    const titre = edit.querySelector(".spanTitre").innerText;
+    const date = edit.querySelector(".spanDate").innerText;
+    e.parentElement.parentElement.remove();
+
+    document.getElementById("titre").value = titre;
+    document.getElementById("date").value = date;
+    document.getElementById("description").value = description;
+
+    toggleModal();
+};
+
+
+
+// function getTasks() {
+//     let tasks = localStorage.getItem()
+// }
+
+
+// function allStorage() {
+
+//     var values = [],
+//         keys = Object.keys(localStorage),
+//         i = keys.length;
+
+//     while ( i-- ) {
+//         values.push( JSON.parse(localStorage.getItem(keys[i])) );
+//     }
+
+//     return values;
+// }
+
+
+// console.log(allStorage()[0].titre);
 
 
 
